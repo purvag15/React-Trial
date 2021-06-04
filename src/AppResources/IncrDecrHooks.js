165bd2f9abf3d0c7e240f1d29a5f9669bb091306
Purva <gautam.purva@gmail.com>
2021-06-05 02:36:10 +0530

    Wrapping up

--- src/AppResources/IncrDecrHooks.js
+++ src/AppResources/IncrDecrHooks.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import randomcolor from "randomcolor"
 
 function IncrDecrHooks() {
     const [count, setCount] = useState(0)
-    const [answer, setAnswer] = useState("Yes")
+    const [color, setColor] = useState("")
 
     function increment() {
         setCount(prevCount => prevCount + 1)
@@ -11,6 +12,13 @@ function IncrDecrHooks() {
     function decrement() {
         setCount(prevCount => prevCount - 1)
     }
+
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [count])
+
+    //if 2nd parameter is an empty array, useEffect works as componentDidMount
+    //else, as soon as any element of the array changes, useEffect comes into play (i.e. componentDidUpdate)
 
     return (
         <div><br/>
